2714734b2b7f0286a33121c68d55db69eca73404
RohanLodhi01 <rohanlodhi51@gmail.com>
2025-11-22 18:59:31 +0530

    completed working with functions

---
+++ 08-functions/src/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const App = () => {
+  function inputMessage(val) {
+    console.log(val);
+  }
+
+  const onScrolling = (elem)=>{
+    if(elem > 0){
+      console.log('seedha scrolling')
+    }else{
+      console.log('ulat scrolling')
+    }
+  }
+  return (
+    <div>
+      {/* <input
+        onChange={function (elem) {
+          // console.log(elem.target.value)
+
+          inputMessage(elem.target.value)
+        }}
+        type="password"
+        placeholder="Enter here"
+      ></input> */}
+      <div onWheel={(elem)=>{
+        onScrolling(elem.deltaY)
+      }}>
+        <div className="page1"></div>
+        <div className="page2"></div>
+        <div className="page3"></div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
