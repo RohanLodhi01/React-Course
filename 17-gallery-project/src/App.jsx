4c2ed674fbfabc1c97459b45b02e96fd7066ee53
RohanLodhi01 <rohanlodhi51@gmail.com>
2025-12-06 19:27:30 +0530

    setup the api call get data

--- 17-gallery-project/src/App.jsx
+++ 17-gallery-project/src/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
 const App = () => {
+  const [userData, setUserData] = useState([]);
+  const getData = async () => {
+    const response = await axios.get(
+      "https://picsum.photos/v2/list?page=2&limit=30"
+    );
+    setUserData(response.data);
+    // console.log(response.data);
+  };
   return (
-    <div className='bg-black h-screen p-4 text-white'>
-      <button className='bg-green-600 px-5 py-2 mb-3  active:scale-95 rounded text-white'>Get Data</button>
+    <div className="bg-black h-screen p-4 text-white">
+      <button
+        onClick={getData}
+        className="bg-green-600 px-5 py-2 mb-3  active:scale-95 rounded text-white"
+      >
+        Get Data
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
